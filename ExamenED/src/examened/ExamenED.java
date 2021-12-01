8069ebdc7275f4f956f5ba2271ebe378d08a3e4c
Mario Tomas <mariotomasdaw@gmail.com>
2021-12-01 17:29:24 +0100

    Reto 2 acabado y capturado

---
+++ ExamenED/src/examened/ExamenED.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examened;
+
+/**
+ *
+ * @author examened
+ */
+public class ExamenED {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println(i);
+            
+        }
+        // TODO code application logic here
+    }
+    
+}
